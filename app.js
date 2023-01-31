bbec062718ee184381ac56453930251cac30ac1c
Mithun <msovik399@gmail.com>
2023-01-31 19:05:20 +0600

    api fetch,time= 21:21

---
+++ app.js
@@ -0,0 +1,24 @@
+const app=Vue.createApp({
+   
+    data(){
+        return{
+            name:'Mithun',
+            email:'ms@gmail.com',
+            gender:'male',
+            picture:'img.jpg'
+        }
+    },
+    methods: {
+       async getUser(){
+        const res=await fetch('https://randomuser.me/api')
+        const {results} = await res.json()
+        console.log(res)
+
+            this.name=results[0].name;
+            this.email=results[0].email;
+            this.gender=results[0].gender;
+            this.picture=results[0].picture.large
+        }
+    },
+})
+app.mount('#app')
