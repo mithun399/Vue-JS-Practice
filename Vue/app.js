7953c2022ae1d443376be06a984699c435354c51
Mithun <msovik399@gmail.com>
2023-02-08 14:52:25 +0600

    outputting list (v-for)

--- Vue/app.js
+++ Vue/app.js
@@ -2,9 +2,18 @@ const app=Vue.createApp({
     data() {
         return {
             showbooks:true,
-            title:'Learn Vue ',
-            author:'Mithun',
-            age:'24'
+            // title:'Learn Vue ',
+            // author:'Mithun',
+            // age:'24',
+            x:0,
+            y:0,
+
+            books:[
+                {title: 'name of the wind',author:'Mithun',age:24},
+                {title: 'the way of king',author:'Sonia',age:22},
+                {title: 'the final empire',author:'MS',age:23},
+
+            ]
         }
     },
     methods: {
@@ -17,8 +26,15 @@ const app=Vue.createApp({
         toggleShow(){
             this.showbooks=! this.showbooks;
         },
-        handleEvent(e){
+        handleEvent(e,data){
             console.log(e,e.type)
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMouse(e){
+            this.x=e.offsetX
+            this.y=e.offsetY
         }
     },
 
