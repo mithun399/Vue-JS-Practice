1cdacec4767bc8efc6b0401f60dc0c578efd50d6
Mithun <msovik399@gmail.com>
2023-02-08 17:03:36 +0600

    Dynamic Classes

--- Vue/app.js
+++ Vue/app.js
@@ -10,9 +10,10 @@ const app=Vue.createApp({
             y:0,
 
             books:[
-                {title: 'name of the wind',author:'Mithun',age:24 , img:'img/book.jpg'},
-                {title: 'the way of king',author:'Sonia',age:22 , img:'img/book1.jpg'},
-                {title: 'the final empire',author:'MS',age:23, img:'img/images.jpeg'},
+                // {title: 'Name' , author:'Author',age:'Age',img:'Image',isFave:false},
+                {title: 'Name of the wind',author:'Mithun',age:24 , img:'img/book.jpg', isFav:true},
+                {title: 'The way of king',author:'Sonia',age:22 , img:'img/book1.jpg' , isFav:false},
+                {title: 'The final empire',author:'MS',age:23, img:'img/images.jpeg' , isFav:true},
 
             ]
         }
