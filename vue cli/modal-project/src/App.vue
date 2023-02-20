c7ea942f1e942cadbc1e1f2d16f704de30d1cc5d
Mithun <msovik399@gmail.com>
2023-02-20 11:42:47 +0600

    Template refs

---
+++ vue cli/modal-project/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+ <h1> {{title}}</h1>
+
+ <input type="text" ref="name">
+ <button @click="handleClick">Ckick Me</button>
+</template>
+
+<script>
+
+
+
+export default{
+  name: 'App',
+  data(){
+    return{
+    title: 'My First Vue Cli App :)'
+    }
+  },
+  methods: {
+    handleClick(){
+    console.log(this.$refs.name)
+    this.$refs.name.classList.add('active')
+    this.$refs.name.focus()
+  }
+}
+}
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  display: inline-block;
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 10px;
+}
+  
+</style>
