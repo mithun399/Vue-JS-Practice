c1f1212042c9cd953042cb40d01776fdc9097bbe
Mithun <msovik399@gmail.com>
2023-02-20 13:40:55 +0600

    Multiple components

--- vue cli/modal-project/src/App.vue
+++ vue cli/modal-project/src/App.vue
@@ -1,28 +1,31 @@
 <template>
  <h1> {{title}}</h1>
 
- <input type="text" ref="name">
- <button @click="handleClick">Ckick Me</button>
+ <!-- <input type="text" ref="name">
+ <button @click="handleClick">Ckick Me</button> -->
+ <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 
 
 
 export default{
   name: 'App',
+  components: { Modal },
   data(){
     return{
     title: 'My First Vue Cli App :)'
     }
   },
-  methods: {
-    handleClick(){
-    console.log(this.$refs.name)
-    this.$refs.name.classList.add('active')
-    this.$refs.name.focus()
-  }
-}
+//   methods: {
+//     handleClick(){
+//     console.log(this.$refs.name)
+//     this.$refs.name.classList.add('active')
+//     this.$refs.name.focus()
+//   }
+// }
 }
 
 
